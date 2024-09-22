f2a8bb0d5e7e768cc216737a81ef104b673529ec
cowkami <lee.unseo17@gmail.com>
2024-09-22 20:43:36 +0900

    wip

--- api-client/src/bin/test_gpt.rs
+++ api-client/src/bin/test_gpt.rs
@@ -3,6 +3,8 @@ use api_client::gpt::Gpt;
 #[tokio::main]
 async fn main() {
     let llm_client = Gpt::new().expect("Failed to initialize OpenAI API client");
-    let response = llm_client.chat("レシピのアイデアを10個考えてちょ").await;
+    let response = llm_client
+        .chat("レシピのアイデアを10個考えてちょ".to_string())
+        .await;
     println!("{:#?}", response);
 }
--- api-client/src/bin/test_line.rs
+++ api-client/src/bin/test_line.rs
@@ -5,7 +5,7 @@ async fn main() {
     let message_client = Line::new().expect("Failed to initialize Line API client");
     let response = message_client
         .reply(
-            "Hello, how are you?",
+            "Hello, how are you?".to_string(),
             "13912a8f297d4fa5bea46f5bcd03727a".to_string(),
         )
         .await;
